ea03dc01ef4d72063277635831f259831b98ad60
DiazPT <diazpt7@gmail.com>
2017-11-29 15:02:55 +0000

    V4

--- backend/database/models.js
+++ backend/database/models.js
@@ -12,89 +12,171 @@ var SystemError = mongoose.model('system_errors', {
   name : String
 });
 
-/* MongoDB Devices' Type's model */
-var DeviceType = mongoose.model('device_types', {
-  id_device_type : String,
-  name : String
+/* MongoDB History' model */
+var History = mongoose.model('history', {
+    Name : String,
+    Producer_ID : Double,
+    Description : String,
+    Date : String
 });
 
-/* MongoDB Products' Model's model */
-var ProductModel = mongoose.model('product_models', {
-  id_product_models : String,
-  name : String,
-  stock : String,
-  sells: String,
-});
+/*
+TYPE OF USERS
+ */
 
-/* MongoDB Usage's model */
-var Usage = mongoose.model('usages', {
-  system_error : String,
-  state : String,
-  time_interval : String,
-  started_timestamp : String,
-  ended_timestamp : String
+/* MongoDB Manager' model */
+var Manager = mongoose.model('manager', {
+    Name : String,
+    Description : String,
+    Username : String,
+    Password : String,
+    Email : String,
+    Token : String,
+    Last_Login : String,
 });
 
 /* MongoDB Users' model */
 var User = mongoose.model('users', {
-  date_registered : String,
-  name : String,
-  password : String,
-  username : String,
-  email : String,
-  devices : [String],
-  token: String
+    Name : String,
+    Description : String,
+    Username : String,
+    Password : String,
+    Email : String,
+    Token : String,
+    Last_Login : String,
+    Date_Registered : String,
 });
 
 /* MongoDB Producers' model */
 var Producer = mongoose.model('producers', {
-  date_registered : String,
-  name : String,
-  password : String,
-  username : String,
-  email : String,
-  device_models : [String],
-  device_types : [String],
-  devices: [String],
-  token : String
+  Name : String,
+  Description : String,
+  Username : String,
+  Password : String,
+  Email : String,
+  Token : String,
+  Last_Login : String,
+  Date_Registered : String,
 });
 
-/* MongoDB Device's model */
-var Device = mongoose.model('devices', {
-  date_registered : String,
-  current_state : String,
-  system_error : String,
-  name : String,
-  device_type : String,
-  producer : String,
-  model : String,
-  readings : [String],
-  usage_history : [String],
-  user : String
+/*
+BUILDINGS
+ */
+
+/* MongoDB Building_Energy_Monthly' model */
+var Building_Energy_Monthly = mongoose.model('building_energy_monthly', {
+    Building_ID : Double,
+    Description : String,
+    E_Consume : Double,
+    E_Consume_Cost : Double,
+    E_Consume_Heating : Double,
+    E_Consume_Heating_Cost : Double,
+    E_Consume_Ilumination : Double,
+    E_Consume_Ilumination_Cost : Double,
+    E_Consume_Ilumination_Exterior : Double,
+    E_Consume_Ilumination_Exterior_Cost : Double,
+    Month : Double,
+    Year : Double
 });
 
-/* MongoDB Reading's model */
-var Reading = mongoose.model('readings', {
-    value : String,
-    type : String,
-    timestamp : String
+/* MongoDB Building_Energy_Anual' model */
+var Building_Energy_Anual = mongoose.model('building_energy_anual', {
+    Building_ID : Double,
+    Description : String,
+    E_Consume : Double,
+    E_Consume_Metro : Double,
+    E_Consume_Person : Double,
+    E_Consume_Degree_Day : Double,
+    E_Consume_Cost : Double,
+    E_Consume_Heating : Double,
+    E_Consume_Heating_Metro : Double,
+    E_Consume_Heating_Person : Double,
+    E_Consume_Heating_Metro_Degree_Day : Double,
+    E_Consume_Heating_Cost : Double,
+    E_Consume_Ilumination : Double,
+    E_Consume_Ilumination_Metro : Double,
+    E_Consume_Ilumination_Person : Double,
+    E_Consume_Ilumination_Cost : Double,
+    E_Consume_Ilumination_Exterior : Double,
+    E_Consume_Ilumination_Exterior_Cost : Double,
+    Year : Double
 });
 
-/* MongoDB Reading's model */
-var User_history = mongoose.model('user_history', {
-    username : String,
-    activity : String,
-    time : String
+/*
+PANELS
+ */
+
+/* MongoDB Panel_Visitors_Month' model */
+var Panel_Visitors_Month = mongoose.model('panel_visitors_month', {
+    Name : String,
+    Panel_ID : Double,
+    Visitors : Double,
+    Month : Double,
+    Year : Double
+});
+
+/* MongoDB Panel' model */
+var Panel = mongoose.model('panel', {
+    Name : String,
+    Description : String,
+    Location : String
+});
+
+/* MongoDB Panel_Visitors_Week' model */
+var Panel_Visitors_Week = mongoose.model('panel_visitors_week', {
+    Name : String,
+    Panel_ID : Double,
+    Visitors : Double,
+    Week : Double,
+    Month : Double,
+    Year : Double
+});
+
+/* MongoDB Panel_Visitors_Year' model */
+var Panel_Visitors_Year = mongoose.model('panel_visitors_year', {
+    Name : String,
+    Panel_ID : Double,
+    Visitors : Double,
+    Year : Double
+});
+
+/*
+MOBILE NODES
+ */
+
+/* MongoDB Mobile_Node' model */
+var Mobile_Node = mongoose.model('mobile_node', {
+    Name : String,
+    Description : String,
+    Location : String
+});
+
+/* MongoDB Data_Type' model */
+var Data_Type = mongoose.model('data_type', {
+    Name : String,
+    Description : String,
+    Unit : String
+});
+
+/* MongoDB Data_Regist_Mobile_Month' model */
+var Data_Regist_Mobile_Month = mongoose.model('data_regist_mobile_month', {
+    Data_Type : Double,
+    Panel_ID : Double,
+    Value : Double,
+    Month : Double,
+    Year : Double
+});
+
+/* MongoDB Data_Regist_Mobile_Year' model */
+var Data_Regist_Mobile_Year = mongoose.model('data_regist_mobile_year', {
+    Data_Type : Double,
+    Panel_ID : Double,
+    Value : Double,
+    Year : Double
 });
 
 module.exports.State = State;
 module.exports.SystemError = SystemError;
-module.exports.DeviceType = DeviceType;
-module.exports.ProductModel = ProductModel;
-module.exports.Usage = Usage;
 module.exports.User = User;
 module.exports.Producer = Producer;
-module.exports.Device = Device;
-module.exports.Reading = Reading;
-module.exports.User_history = User_history;
 module.exports.mongoose = mongoose;
